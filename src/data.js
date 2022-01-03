73ec3fc0947ad74bdd4ba0f14670e3e201c37c60
RobbeDierickx <44427819+RobbeDierickx@users.noreply.github.com>
2022-01-03 15:30:52 +0100

    Oef 1

--- src/data.js
+++ src/data.js
@@ -1,22 +1,27 @@
-const uuid = require('uuid');
+const uuid = require("uuid");
 
 const stores = [
-  {
-    id: 'cc406ed9-fc02-4185-b073-8c12b61b5c79'
-    name: 'Den Olijfboom',
-  },
-  {
-    id: '5f2919aa-333a-4745-8166-3002ab30de0e',
-    name: 'Pizza Talia'
-  }
+    {
+        id: "cc406ed9-fc02-4185-b073-8c12b61b5c79",
+        name: "Den Olijfboom",
+    },
+    {
+        id: "5f2919aa-333a-4745-8166-3002ab30de0e",
+        name: "Pizza Talia",
+    },
 ];
 
-export function createStore({ name }) {
-  const newStore = { id: uuid(), name };
-  stores.push(newStore);
-  return newStore;
+function createStore({ name }) {
+    const newStore = { id: uuid(), name };
+    stores.push(newStore);
+    return newStore;
 }
 
-export function getStores() {
-  return stores;
+function getStores() {
+    return stores;
 }
+
+module.exports = {
+    getStores,
+    createStore,
+};
